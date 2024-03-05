ce88d53c686521e1cf6f2461a24f1a90bf53f071
CWTWIS <chawanrat.wis@gmail.com>
2024-03-05 14:51:32 +0700

    modified seed.js and change database

--- src/repository/roomFacility.js
+++ src/repository/roomFacility.js
@@ -1,6 +1,6 @@
 const prisma = require("../config/prisma")
 
 // =========================================== BASIC CRUD ===================================
-module.exports.createRoomUtil = async (data) => await prisma.roomUtil.create({ data })
+module.exports.createRoomFacility = async (data) => await prisma.roomFacility.create({ data })
 
 // =========================================== CUSTOM REPOSITORY ===================================
