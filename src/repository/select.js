0b28e6bfcccac69a274b06671f13f9d2efc05f20
CWTWIS <chawanrat.wis@gmail.com>
2024-03-05 15:38:21 +0700

    fetch facilities

--- src/repository/select.js
+++ src/repository/select.js
@@ -2,3 +2,4 @@ const prisma = require("../config/prisma")
 
 module.exports.getDistricts = async () => prisma.district.findMany({ select: { id: true, district: true } })
 module.exports.getProvinces = async () => prisma.province.findMany({ select: { id: true, province: true } })
+module.exports.getFacilities = async () => prisma.facility.findMany({ select: { id: true, name: true } })
