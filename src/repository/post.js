adb450d1e51e261bf819144f34db70315811a02c
Thirawoot-put <thirawoot.put@gmail.com>
2024-03-11 15:02:56 +0700

    config-repo-for-post-detail

--- src/repository/post.js
+++ src/repository/post.js
@@ -58,7 +58,16 @@ module.exports.getActivePosts = async () =>
 module.exports.getPostByPostId = async (postId) =>
     await prisma.post.findFirst({
         where: { id: postId },
-        include: { user: true, room: { include: { condo: true, roomImages: true, roomFacilities: { include: { facility: true } } } } },
+        include: {
+            user: true,
+            room: {
+                include: {
+                    condo: { include: { district: true, province: true } },
+                    roomImages: true,
+                    roomFacilities: { include: { facility: true } },
+                },
+            },
+        },
     })
 
 // module.exports.getPostsByUserId = async (userId) =>
